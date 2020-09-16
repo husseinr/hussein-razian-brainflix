f3f3b470426530eb8bbabc6d7b1c0443d5e5cb8a
Hussein Razian <husseinrazian@outlook.com>
2020-09-16 00:24:28 -0600

    Started Styling and Adding Main Page Componenets

---
+++ sprint one/src/components/PageHeader/PagerHeader.jsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import './pageHeader.scss';
+
+// const PageHeader = () => {
+//     return (
+//      <h1 className="main-header">BMX Rampage: 2018 Highlights</h1>);
+// }
+
+// export default PageHeader;
