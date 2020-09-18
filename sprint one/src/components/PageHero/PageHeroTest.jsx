e2f12c8b9d7d95a0c7886c9fda5939283359f6be
Hussein Razian <husseinrazian@outlook.com>
2020-09-18 14:51:33 -0600

    Moved Git Ignore File and Node Module

--- sprint one/src/components/PageHero/PageHeroTest.jsx
+++ sprint one/src/components/PageHero/PageHeroTest.jsx
@@ -23,54 +23,115 @@ import './pageHero.scss';
 // };
 
 class PageCurrentVideo extends React.Component { 
-state = currentVideo = {
+state = 
 
-    id: '020',
-    title: 'BMC Rampage:2018 Highlights',
-    description: 'On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title',
-    channel: 'Red Cow',
-    image: {VideoPlaceholder},
-    views: '1,001,023',
-    likes: '110,985',
-    duration: 'type of <string>',
-    video: 'type of <string>',
-    timestamp: '4:00',
-    comments: [
+    currentVideo = {
 
-            {
-                name: "Michael Lyons",
-                date: "12/18/2018",
-                comment:`They BLEW the ROOF off at their
-                last show, once everyone started
-                figuring out they were going. This is
-                still simply the greatest opening of a
-                concert I have EVER witnessed.`,
-                id: '001'
-            },
+        id: '020',
+        title: 'BMC Rampage:2018 Highlights',
+        description: 'On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title',
+        channel: 'Red Cow',
+        image: {VideoPlaceholder},
+        views: '1,001,023',
+        likes: '110,985',
+        duration: 'type of <string>',
+        video: 'type of <string>',
+        timestamp: '4:00',
+        comments: [
+
+                {
+                    name: "Michael Lyons",
+                    date: "12/18/2018",
+                    comment:`They BLEW the ROOF off at their
+                    last show, once everyone started
+                    figuring out they were going. This is
+                    still simply the greatest opening of a
+                    concert I have EVER witnessed.`,
+                    id: '001'
+                },
+                
+                {
+                    name: "Gary Wong",
+                    date: "12/12/2018",
+                    comment:`Every time I see him shred I feel so
+                    motivated to get off my couch and
+                    hop on my board. He’s so talented! I
+                    wish I can ride like him one day so I
+                    can really enjoy myself!`,
+                    id: '002'
+                },
             
+                {
+                    name: "Theodore Duncan",
+                    date: "11/15/2018",
+                    comment:`How can someone be so good!!!
+                    You can tell he lives for this and
+                    loves to do it every day. Everytime I
+                    see him I feel instantly happy! He’s
+                    definitely my favorite ever!`,
+                    id: '003'
+                },    
+            ]
+        };
+
+        upComingVideo = [
+
             {
-                name: "Gary Wong",
-                date: "12/12/2018",
-                comment:`Every time I see him shred I feel so
-                motivated to get off my couch and
-                hop on my board. He’s so talented! I
-                wish I can ride like him one day so I
-                can really enjoy myself!`,
-                id: '002'
+                photo: TravelPro,
+                title: 'Become A Travel Pro In One Easy Lesson',
+                channel: 'Scotty Cranmer',
+                id:'010',
             },
         
             {
-                name: "Theodore Duncan",
-                date: "11/15/2018",
-                comment:`How can someone be so good!!!
-                You can tell he lives for this and
-                loves to do it every day. Everytime I
-                see him I feel instantly happy! He’s
-                definitely my favorite ever!`,
-                id: '003'
-            },    
-        ] 
-    };
+                photo: LesHouches,
+                title: 'Les Houches The Hidden Gem Of The',
+                channel: 'Scotty Cranmer',
+                id:'011',
+            },
+        
+            {
+                photo: TravelHealth,
+                title: 'Travel Health Useful Medical Information',
+                channel: 'Scotty Cranmer',
+                id:'012',
+            },
+        
+            {
+                photo: CheapAirline,
+                title: 'Cheap Airline Tickets Great Ways To Save',
+                channel: 'Emily Harper',
+                id:'013',
+            },
+        
+            {
+                photo: RomanticBreak,
+                title: 'Take A Romantic Break In A Boutique Hotel',
+                channel: 'Ethan Owen',
+                id:'014',
+            },
+        
+            {
+                photo: PerfectAccommodations,
+                title: 'Choose The Perfect Accommodations',
+                channel: 'Lydia Perez',
+                id:'015',
+            },
+        
+            {
+                photo: CruisingDestination,
+                title: 'Crusing Destination Ideas',
+                channel: 'Timothy Austin',
+                id:'016',
+            },
+        
+            {
+                photo: TrainTravel,
+                title: 'Train Travel On Track For Safety',
+                channel: 'Scotty Cranmer',
+                id:'017',
+            },
+        ]
 
         render() {
 
